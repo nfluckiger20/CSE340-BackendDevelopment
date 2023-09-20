9ab6c95e74e8f8891913ff808c5020a034e01ef4
nfluckiger20 <flu20002@byui.edu>
2023-09-19 19:22:18 -0600

    pnpm run dev

--- server.js
+++ server.js
@@ -21,7 +21,11 @@ app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(require("./routes/static"))
+//Index route
+app.get("/", function(req,res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
