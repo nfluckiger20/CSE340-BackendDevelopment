30fe5d2f80b5fdc4b737b9aed1f2455a63b920b0
nfluckiger20 <flu20002@byui.edu>
2023-10-17 14:58:01 -0600

    baseController

--- utilities/index.js
+++ utilities/index.js
@@ -60,3 +60,10 @@ Util.buildClassificationGrid = async function(data){
     }
     return grid
   }
+
+  /* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
