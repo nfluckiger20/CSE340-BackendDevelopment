1710141a90740d3bbc5e5b3ce6846cb8e798879e
nfluckiger20 <flu20002@byui.edu>
2023-11-02 19:29:26 -0600

    activities - almost done

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,6 +4,10 @@ const express = require("express")
 const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const { buildLogin } = require('../controllers/accountController');
+const { errorHandler } = require('../controllers/accountController');
+const regValidate = require('../utilities/account-validation')
+
 
 // Deliver login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
@@ -15,4 +19,20 @@ router.get("/register", utilities.handleErrors(accountController.buildRegister))
 router.get('/', buildLogin);
 router.use(errorHandler);
 
-module.exports = router
+// Process the login attempt
+router.post(
+    "/login",
+    (req, res) => {
+      res.status(200).send('login process')
+    }
+  ),
+  
+  // Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
+
+module.exports = {router}
